9f0e23d1ea4b5fdd65b825fe561f04e9a4f962bc
achristoforides <38366659+achristoforides@users.noreply.github.com>
2019-04-13 05:30:50 -0400

    Add point list to brushes, fixed x and y issue with getting/setting pixels, implemented PaintStroke (not well)

--- brush_stroke.py
+++ brush_stroke.py
@@ -1,11 +1,17 @@
 # A wrapper class to hold all data which comprises a brush stroke
+#
+# radius: the radius of the brush
+# color:  the color of the stroke
+# points: the points added to the stroke, stores tuples (x, y)
 class BrushStroke:
     # radius : positive, floating point number
-    def __init__(self, radius = 0):
+    def __init__(self, radius = 0, color = (0, 0, 0)):
         if radius < 0.0:
             self.radius = 0.0
             return
         self.radius = radius
+        self.color = color
+        self.points = []
 
     # Returns the current radius of the BrushStroke
     def getRadius(self):
@@ -18,3 +24,16 @@ class BrushStroke:
             self.radius = 0.0
             return
         self.radius = r
+
+    # Returns the current color of the BrushStroke
+    def getColor(self):
+        return self.color
+
+    # Sets the color of the current BrushStroke to the parameter passed
+    # in, color should be a 3D tuple in the following format: (B, G, R)
+    def setColor(self, color):
+        self.color = color
+
+    # Adds the specified (x, y) point to the points list
+    def addPoint(self, x, y):
+        self.points.append((x, y))
